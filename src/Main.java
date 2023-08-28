236c7804519d3fee73cebde420e8f28c88bd51a9
Fran Borges <borgesfran988@gmail.com>
2023-08-28 20:53:54 -0300

    class position created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Position pos = new Position(5, 5);
+        System.out.println(pos);
+    }
+}

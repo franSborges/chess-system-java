68ba5b43ea08291cdeb7e66143d1a12ccd44177a
Fran Borges <borgesfran988@gmail.com>
2023-08-28 21:10:09 -0300

    class Board  created

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
         
-        Position pos = new Position(5, 5);
-        System.out.println(pos);
+       Board board = new Board(3, 5);
+       System.out.println(board);
     }
 }

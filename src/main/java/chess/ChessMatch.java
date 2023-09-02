2ea386496038cbbeac3ba5c58913099aa6f4d403
Fran Borges <borgesfran988@gmail.com>
2023-09-02 20:32:48 -0300

    ChessPosition constructor refatoring

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,6 +1,6 @@
 package chess;
 import boardgame.Board;
-import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -29,8 +29,8 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('6', 6, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('e', 1,new Rook(board, Color.WHITE));
 
     }
--- src/main/java/chess/ChessPosition.java
+++ src/main/java/chess/ChessPosition.java
@@ -7,7 +7,7 @@ public class ChessPosition {
   private char column;
   private int row;
 
-  public ChessPosition(char c, int i) {
+  public ChessPosition(char column, int row) {
     if (column < 'a' || column > 'h' || row < 1 || row > 8) {
       throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
     }

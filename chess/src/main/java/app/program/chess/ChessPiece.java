ada90899d21b58c78c269646c7510feeff329ada
Fran Borges <borgesfran988@gmail.com>
2023-09-10 15:31:34 -0300

    Rook.PossibleMoves ok

--- chess/src/main/java/app/program/chess/ChessPiece.java
+++ chess/src/main/java/app/program/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package app.program.chess;
 
 import app.program.boardgame.Board;
 import app.program.boardgame.Piece;
+import app.program.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -15,4 +16,10 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
+    }
+
 }

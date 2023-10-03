2f3996411def309a0463e5125f153db50c31a1f6
Fran Borges <borgesfran988@gmail.com>
2023-10-03 16:05:31 -0300

    :feat: Feature check logic implemented

--- chess/src/main/java/app/program/Main.java
+++ chess/src/main/java/app/program/Main.java
@@ -33,6 +33,7 @@ public class Main {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
--- chess/src/main/java/app/program/UI.java
+++ chess/src/main/java/app/program/UI.java
@@ -55,6 +55,9 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
